4debc815005a5c69acb407eb50212cb9b496117f
eto1302 <martinangelov1302@gmail.com>
2017-10-11 19:53:09 +0300

    These are the exercises for the Arrays lecture from the SoftUni course Programming Fundamentals

---
+++ 04_SoftUni_ProgrammingFundamentals_Arrays/Compare Char Arrays/Compare Char Arrays.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+namespace Compare_Char_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+            char[] s1 = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+            int flag = 8;
+            for (int i = 0; i < Math.Min(s.Length,s1.Length); i++)
+            {
+                if (s[i] < s1[i]) flag = 1;
+                else if (s[i] > s1[i]) flag = 2;
+                else if (s[i] == s1[i]) continue;
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine($"{string.Join("", s)}");
+                Console.WriteLine($"{string.Join("", s1)}");
+            }
+            else if(flag == 2)
+            {
+                
+                    Console.WriteLine($"{string.Join("", s1)}");
+                    Console.WriteLine($"{string.Join("", s)}");
+                
+            }
+            else if(flag == 8)
+            {
+                if(s.Length<=s1.Length)
+                {
+                    Console.WriteLine($"{string.Join("", s)}");
+                    Console.WriteLine($"{string.Join("", s1)}");
+                }
+                if (s.Length > s1.Length)
+                {
+                    Console.WriteLine($"{string.Join("", s1)}");
+                    Console.WriteLine($"{string.Join("", s)}");
+                }
+            }
+        }
+    }
+}
---
+++ 04_SoftUni_ProgrammingFundamentals_Arrays/Most Frequent Number/Most Frequent Number.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Linq;
+
+
+namespace Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var br1 = 0;
+            var n = 0;
+            var br = new int[65535];
+            var k = 0;
+            
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                br[numbers[i]]++;
+            }
+
+            var max = br.Max();
+            for (int i = 0; i < br.Length; i++)
+            {
+                if (max == br[i])
+                {
+                    n = i;
+                    br1++;
+
+                }
+            }
+
+            if (br1 > 1)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+
+                    if (numbers[j] == n)
+                    {
+                        if (j < k)
+                        {
+                            k = j;
+                        }
+
+
+                    }
+                }
+                Console.WriteLine(numbers[k]);
+            }
+
+            else Console.WriteLine(n);
+
+
+        }
+    }
+}
+
