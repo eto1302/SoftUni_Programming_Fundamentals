0b52f4cf7fb036c872a62497ee57a2a6458ba066
eto1302 <martinangelov1302@gmail.com>
2017-10-14 18:04:23 +0300

    These are the exercises from the Lists lecture from the Softuni course Programming Fundamentals

---
+++ 05_SoftUni_ProgrammingFundamentals_Lists/Bomb Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        List<int> sequence = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        string[] intpit = Console.ReadLine().Split();
+
+        int number = int.Parse(intpit[0]);
+        int power = int.Parse(intpit[1]);
+
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            if (sequence[i] == number)
+            {
+                int left = Math.Max(i - power, 0);
+
+                int right = Math.Min(i + power, sequence.Count - 1);
+
+                int length = right - left + 1;
+                sequence.RemoveRange(left, length);
+                i = 0;
+            }
+        }
+        Console.WriteLine(sequence.Sum());
+    }
+}
---
+++ 05_SoftUni_ProgrammingFundamentals_Lists/Sum Reversed Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Sum_Reversed_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int n=0;
+            int sum = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                while(list[i]!=0)
+                {
+                    n = n * 10 + list[i] % 10;
+                    list[i] = list[i] / 10;
+                }
+               
+                sum = sum + n;
+                n = 0;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
