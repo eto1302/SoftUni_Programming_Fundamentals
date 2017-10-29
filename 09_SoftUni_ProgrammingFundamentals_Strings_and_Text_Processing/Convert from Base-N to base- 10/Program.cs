abe967bf975203f25e6f842a8c251af03fe6b5f4
eto1302 <martinangelov1302@gmail.com>
2017-10-29 20:08:06 +0200

    These are the exercises from the lecture "Strings and Text Processing" from the SoftUni course "Programming Fundamentals"

---
+++ 09_SoftUni_ProgrammingFundamentals_Strings_and_Text_Processing/Convert from Base-N to base- 10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Convert_from_Bse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ').ToArray();
+            uint n = uint.Parse(s[0]);
+            BigInteger a = BigInteger.Parse(s[1]);
+            BigInteger res = 0;
+            BigInteger br = 0;
+            BigInteger l = 1;
+            if (n == 10) Console.WriteLine(s[1]);
+            else
+            {
+                for (int i = s[1].Length - 1; i >= 0; i--)
+                {
+                    for (int i1 = 0; i1 < br; i1++)
+                    {
+                        l = l * n;
+                    }
+
+                    res = res + BigInteger.Parse(s[1][i].ToString()) * l;
+                    l = 1;
+                    br++;
+
+                }
+                Console.WriteLine(res);
+
+            }
+
+
+        }
+    }
+}
